567db904ffa1de78dfa07ab51358421aa8b54331
Michał Górny <mgorny@gentoo.org>
2010-11-03 16:32:12 +0100

    Start splitting flaggie into modules, add distutils.

---
+++ lib/flaggie/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#	vim:fileencoding=utf-8
+# (C) 2010 Michał Górny, distributed under the terms of 3-clause BSD license
+
+PV='0'
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#	vim:fileencoding=utf-8
+# (c) 2010 Michał Górny <mgorny@gentoo.org>
+# Released under the terms of the 3-clause BSD license.
+
+from distutils.core import setup
+
+import os.path, sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+try:
+	from flaggie import PV
+except ImportError:
+	PV = 'unknown'
+
+setup(
+		name = 'flaggie',
+		version = PV,
+		author = 'Michał Górny',
+		author_email = 'mgorny@gentoo.org',
+		url = 'http://github.com/mgorny/flaggie',
+
+		package_dir = {'': 'lib'},
+		packages = ['flaggie'],
+		scripts = ['flaggie'],
+
+		classifiers = [
+			'Development Status :: 3 - Alpha',
+			'Environment :: Console',
+			'Intended Audience :: System Administrators',
+			'License :: OSI Approved :: BSD License',
+			'Operating System :: POSIX',
+			'Programming Language :: Python',
+			'Topic :: System :: Installation/Setup'
+		]
+)
