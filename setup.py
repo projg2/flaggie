c34557493fa01558edcb1c1bf02cee98085c6bb7
Michał Górny <mgorny@gentoo.org>
2010-11-15 09:18:17 +0100

    Bump PV to 0.0.2.2.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
 		scripts = ['flaggie'],
 
 		classifiers = [
-			'Development Status :: 3 - Alpha',
+			'Development Status :: 4 - Beta',
 			'Environment :: Console',
 			'Intended Audience :: System Administrators',
 			'License :: OSI Approved :: BSD License',
