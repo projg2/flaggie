348e93bf747b42ab857020df871162f11423e983
Michał Górny <mgorny@gentoo.org>
2023-01-06 19:32:23 +0100

    Add package.* parsing code
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- flaggie/config.py
+++ flaggie/config.py
@@ -1,8 +1,9 @@
-# (c) 2022 Michał Górny
+# (c) 2022-2023 Michał Górny
 # Released under the terms of the MIT license
 
 import enum
 import os
+import re
 import typing
 
 from pathlib import Path
@@ -12,9 +13,18 @@ class TokenType(enum.IntEnum):
     USE_FLAG = enum.auto()
 
 
+class ConfigLine(typing.NamedTuple):
+    package: typing.Optional[str] = None
+    flat_flags: list[str] = []
+    grouped_flags: list[tuple[str, list[str]]] = []
+    comment: typing.Optional[str] = None
+
+
 CONFIG_FILENAMES = {
     TokenType.USE_FLAG: "package.use",
 }
+
+COMMENT_RE = re.compile(r"(?<!\S)#(.*)$")
 
 
 def find_config_files(config_root: Path, token_type: TokenType) -> list[Path]:
@@ -55,3 +65,36 @@ def find_config_files(config_root: Path, token_type: TokenType) -> list[Path]:
 
     # otherwise (presumably it's a file), use the path directly
     return [path]
+
+
+def parse_config_file(lines: list[str]
+                      ) -> typing.Generator[ConfigLine, None, None]:
+    """
+    Parse config file data
+
+    Parse the config file data supplied as list of lines into a list
+    of corresponding ConfigLine objects.
+    """
+
+    for line in lines:
+        line = line.rstrip()
+        comment_m = COMMENT_RE.search(line)
+        if comment_m is not None:
+            line = line[:comment_m.start()]
+
+        split = line.split()
+        group: tuple[str, list[str]] = ("", [])
+        groups = [group]
+        for flag in split[1:]:
+            if flag.endswith(":"):
+                group = (flag[:-1], [])
+                groups.append(group)
+                continue
+            group[1].append(flag)
+
+        assert groups[0][0] == ""
+        yield ConfigLine(
+            package=split[0] if split else None,
+            flat_flags=groups[0][1],
+            grouped_flags=groups[1:],
+            comment=comment_m.group(1) if comment_m is not None else None)
--- test/test_config.py
+++ test/test_config.py
@@ -1,9 +1,11 @@
-# (c) 2022 Michał Górny
+# (c) 2022-2023 Michał Górny
 # Released under the terms of the MIT license
 
 import pytest
 
-from flaggie.config import TokenType, find_config_files
+from flaggie.config import (TokenType, ConfigLine, find_config_files,
+                            parse_config_file,
+                            )
 
 
 @pytest.mark.parametrize(
@@ -31,3 +33,28 @@ def test_find_config(tmp_path, layout, expected):
         expected = layout
     assert find_config_files(tmp_path, TokenType.USE_FLAG
                              ) == [confdir / x for x in expected]
+
+
+def test_parse_config_file():
+    config = [
+        "#initial comment\n",
+        "  # comment with whitespace\n",
+        "\n",
+        "*/* foo bar baz # global flags\n",
+        "*/* FROBNICATE_TARGETS: frob1 frob2\n",
+        "  dev-foo/bar weird#flag other # actual comment # more comment\n",
+        "dev-foo/baz mixed LONG: too EMPTY:\n"
+    ]
+
+    assert list(parse_config_file(config)) == [
+        ConfigLine(comment="initial comment"),
+        ConfigLine(comment=" comment with whitespace"),
+        ConfigLine(),
+        ConfigLine("*/*", ["foo", "bar", "baz"], [], " global flags"),
+        ConfigLine("*/*",
+                   grouped_flags=[("FROBNICATE_TARGETS", ["frob1", "frob2"])]),
+        ConfigLine("dev-foo/bar", ["weird#flag", "other"], [],
+                   " actual comment # more comment"),
+        ConfigLine("dev-foo/baz", ["mixed"],
+                   [("LONG", ["too"]), ("EMPTY", [])]),
+    ]
