81adf01b808b49f7757219edb085c68fab4ed1da
Michał Górny <mgorny@gentoo.org>
2023-01-08 15:04:55 +0100

    mangle: Move flag matching into a function
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- flaggie/mangle.py
+++ flaggie/mangle.py
@@ -40,6 +40,34 @@ def match_packages(config_files: list[ConfigFile],
             yield (config_file, line_no, line)
 
 
+def match_flags(line: ConfigLine,
+                prefix: typing.Optional[str],
+                name: str,
+                ) -> typing.Generator[tuple[typing.Optional[str], list[str],
+                                            int], None, None]:
+    """
+    Yield matching flags from specified config line
+
+    Iterate over flags on the config line, in reverse order and match them
+    against specified (prefix, name) pair.
+
+    Yields a tuple of (matched group name, list instance, list index).
+    """
+
+    # TODO
+    assert prefix is None, "not implemented yet"
+    assert not line.grouped_flags, "not implemented yet"
+
+    for rev_index, line_flag in enumerate(reversed(line.flat_flags)):
+        line_flag = line_flag.lstrip("-")
+        # FIXME: package files only support '*' after '_'
+        # (or in group)
+        if fnmatch.fnmatch(name, line_flag):
+            # 0-based
+            index = len(line.flat_flags) - rev_index - 1
+            yield (None, line.flat_flags, index)
+
+
 def mangle_flag(config_files: list[ConfigFile],
                 package: str,
                 prefix: typing.Optional[str],
@@ -54,30 +82,25 @@ def mangle_flag(config_files: list[ConfigFile],
     def try_inplace() -> bool:
         for config_file, line_no, line in match_packages(config_files, package,
                                                          pkg_is_wildcard):
-            # TODO
-            assert not line.grouped_flags, "not implemented yet"
-
-            for index, line_flag in enumerate(line.flat_flags):
-                line_flag = line_flag.lstrip("-")
-                # FIXME: package files only support '*' after '_'
-                # (or in group)
-                if fnmatch.fnmatch(name, line_flag):
-                    debug_common = (
-                        f"Match found: {config_file.path}:{line_no} "
-                        f"{line.package} {line.flat_flags[index]}")
-                    # if this was an exact match, we can mangle it in place
-                    if line.package == package and line_flag == name:
-                        logging.debug(f"{debug_common}, updating in place")
-                        line.flat_flags[index] = (
-                            name if new_state else f"-{name}")
-                        # 0-based
-                        config_file.modified_lines.add(line_no - 1)
-                        return True
-                    logging.debug(
-                        f"{debug_common}, cannot update in place")
-                    # otherwise, we can't update in-place -- we need to add
-                    # after this wildcard
-                    return False
+            for group_prefix, flag_list, index in match_flags(line, prefix,
+                                                              name):
+                assert group_prefix is None, "not implemented yet"
+                debug_common = (
+                    f"Match found: {config_file.path}:{line_no} "
+                    f"{line.package} {flag_list[index]}")
+                # if this was an exact match, we can mangle it in place
+                matched_flag = flag_list[index].lstrip("-")
+                if line.package == package and matched_flag == name:
+                    logging.debug(f"{debug_common}, updating in place")
+                    flag_list[index] = name if new_state else f"-{name}"
+                    # 0-based
+                    config_file.modified_lines.add(line_no - 1)
+                    return True
+                logging.debug(
+                    f"{debug_common}, cannot update in place")
+                # otherwise, we can't update in-place -- we need to add
+                # after this wildcard
+                return False
         return False
 
     if try_inplace():
