12696e4c49b5b4694d0f6fa2e4cf4965d02b75c1
Michał Górny <mgorny@gentoo.org>
2023-01-12 20:33:09 +0100

    Implement --config-root option
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- flaggie/__main__.py
+++ flaggie/__main__.py
@@ -15,6 +15,11 @@ from flaggie.config import (TokenType, find_config_files, read_config_files,
 
 def main(prog_name: str, *argv: str) -> int:
     argp = argparse.ArgumentParser(prog=os.path.basename(prog_name))
+    argp.add_argument("--config-root",
+                      type=Path,
+                      default=Path("/"),
+                      help="Root directory relative to which configuration "
+                           "files will be processed (default: /)")
     argp.add_argument("--debug",
                       action="store_true",
                       help="Enable debug output")
@@ -23,9 +28,14 @@ def main(prog_name: str, *argv: str) -> int:
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
 
-    config_root = Path("/")  # TODO
+    portage_dir = args.config_root / "etc/portage"
+    if not portage_dir.is_dir():
+        argp.error(
+            f"{portage_dir} does not exist, did you specify correct "
+            "--config-root?")
+
     all_configs = {
-        k: list(read_config_files(find_config_files(config_root, k)))
+        k: list(read_config_files(find_config_files(args.config_root, k)))
         for k in TokenType}
 
     for config_files in all_configs.values():
